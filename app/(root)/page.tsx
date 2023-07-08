183ee8c07cd0550459250ebca13e63434a1cad00
Subhranshu Pati <subhranshupati0412@gmail.com>
2023-07-08 21:06:03 +0530

    Basic Auth with clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
-import Image from 'next/image'
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <h1>Hello admin dashboard</h1>
-  )
+    <>
+      <Button variant="default">Click me</Button>
+    </>
+  );
 }
