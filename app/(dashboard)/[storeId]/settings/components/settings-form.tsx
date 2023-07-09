151abbed1e6cc340d604813f38a4eebbecaa10c2
Subhranshu Pati <subhranshupati0412@gmail.com>
2023-07-09 16:31:50 +0530

    Update and delete

--- app/(dashboard)/[storeId]/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/settings/components/settings-form.tsx
@@ -10,6 +10,9 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { toast } from "react-hot-toast";
+import AlertModal from "@/components/modals/alert-modal";
+import { useRouter } from "next/navigation";
 
 interface SettingFormProps {
     initialData: Store;
@@ -21,7 +24,10 @@ const formSchema = z.object({
 
 type SettingsFormValues = z.infer<typeof formSchema>;
 
+
 const SettingsForm = ({ initialData }: SettingFormProps) => {
+
+    const router = useRouter()
     const [open, setOpen] = useState(false)
     const [loading, setLoading] = useState(false)
 
@@ -31,17 +37,59 @@ const SettingsForm = ({ initialData }: SettingFormProps) => {
     });
 
     const onSubmit = async (values: SettingsFormValues) => {
-        console.log(values);
+        try {
+            setLoading(true)
+            await fetch(`/api/stores/${initialData.id}`, {
+                method: "PUT",
+                body: JSON.stringify(values),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+            router.refresh()
+            toast.success("Store updated successfully!")
+        }
+        catch (err) {
+            toast.error("Something went wrong!")
+        }
+        finally {
+            setLoading(false)
+        }
     }
 
+    const onDelete = async () => {
+        try {
+            setLoading(true)
+            await fetch(`/api/stores/${initialData.id}`, {
+                method: "DELETE",
+            })
+            router.refresh()
+            router.push("/")
+            toast.success("Redirecting...")
+        }
+        catch (err) {
+            toast.error("Make sure you remove all products and categories first!")
+        }
+        finally {
+            setLoading(false)
+            setOpen(false)
+        }
+    }
     return (
         <>
+            <AlertModal
+                isOpen={open}
+                onClose={() => setOpen(false)}
+                onConfirm={onDelete}
+                loading={loading}
+            />
             <div className="flex items-center justify-between">
                 <Heading title="Settings" description="Manage your store settings" />
                 <Button
                     variant="destructive"
+                    disabled={loading}
                     size="icon"
-                    onClick={() => alert("Delete store")}
+                    onClick={() => setOpen(true)}
                 >
                     <Trash className="h-4 w-4" />
                 </Button>
