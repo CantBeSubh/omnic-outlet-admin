f3ec54063a5777a135fafa32cb0cc9f1954c5fcd
Subhranshu Pati <subhranshupati0412@gmail.com>
2023-07-09 15:04:57 +0530

    Settings form

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = async () => {
         <div className="border-b">
             <div className="flex h-16 items-center px-4">
                 <StoreSwitcher items={stores} />
-                <MainNav />
+                <MainNav className="ml-4" />
                 <div className="ml-auto flex items-center space-x-4">
                     <UserButton afterSignOutUrl="/" />
                 </div>
